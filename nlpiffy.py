82a9833cdbb7562c60d408c62400acac09d7eba0
tensilick <tensilick@proton.me>
2022-06-04 14:49:45 +0000

    cheatsheet

--- nlpiffy.py
+++ nlpiffy.py
@@ -42,4 +42,4 @@ def main():
 # Tokenization 
 @main.command()
 @click.argument('text')
-@click.option('--tokentype',help="Specify Type of Tokenization -Word 
+@click.option('--tokentype',help="Specify Type of Tokenization -Word Tokens or Sen
