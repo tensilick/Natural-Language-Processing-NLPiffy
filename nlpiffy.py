91b6f891ece6f9e1c752ad90df012dad158b875c
tensilick <tensilick@proton.me>
2022-07-10 11:13:32 +0000

    mmc: sd: Add retries in re-detection

--- nlpiffy.py
+++ nlpiffy.py
@@ -49,4 +49,4 @@ def tokens(text,tokentype,save):
 	raw_text = TextBlob(text)
 	final_result = raw_text
 	if tokentype == 'word' and save =='True':
-		click.secho('Your tex
+		click.secho('Your text was: {}'.fo
