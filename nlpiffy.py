5d8381bca387fc67122a36bb2076b1268ab2e8df
tensilick <tensilick@proton.me>
2022-07-25 19:44:02 +0000

    Added more tests

--- nlpiffy.py
+++ nlpiffy.py
@@ -51,4 +51,4 @@ def tokens(text,tokentype,save):
 	if tokentype == 'word' and save =='True':
 		click.secho('Your text was: {}'.format(text),fg='yellow')
 		click.secho('Word Tokens: {}'.format(final_result.words),fg='green')
-		save_to_
+		save_to_json(s
