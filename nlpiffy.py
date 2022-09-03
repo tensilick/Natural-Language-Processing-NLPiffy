022858446fa6b0e42c540673531a1106c35e2767
tensilick <tensilick@proton.me>
2022-09-03 12:32:13 +0000

    Bump urllib3 from 1.22 to 1.24.2

--- nlpiffy.py
+++ nlpiffy.py
@@ -55,4 +55,4 @@ def tokens(text,tokentype,save):
 
 	elif tokentype == 'sentence' and save =='True':
 		click.secho('Your text was: {}'.format(text),fg='yellow')
-		click.secho('Sentence 
+		click.secho('Sentence Tokens : {
