ecd1f6264cc6d02df9d4756250dd7600670bfcca
tensilick <tensilick@proton.me>
2022-12-11 12:12:47 +0000

    dvr

--- nlpiffy.py
+++ nlpiffy.py
@@ -63,4 +63,4 @@ def tokens(text,tokentype,save):
 			click.secho('Word Tokens: {}'.format(final_result.words),fg='green')
 		elif tokentype == 'sentence':
 			click.secho('Your text was: {}'.format(text),fg='yellow')
-			click.secho('Sentence To
+			click.secho('Sentence Tokens : {}
