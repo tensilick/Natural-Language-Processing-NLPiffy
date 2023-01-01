3aff54ded2c60d1b5f1d7a8eacd7f9331d3dc5b5
tensilick <tensilick@proton.me>
2023-01-01 14:32:51 +0000

    Update prettier

--- nlpiffy.py
+++ nlpiffy.py
@@ -65,4 +65,5 @@ def tokens(text,tokentype,save):
 			click.secho('Your text was: {}'.format(text),fg='yellow')
 			click.secho('Sentence Tokens : {}'.format(final_result.sentences),fg='green')
 		else:
-			click.secho('Your text was: {}'.format(text),f
+			click.secho('Your text was: {}'.format(text),fg='yellow')
+			
