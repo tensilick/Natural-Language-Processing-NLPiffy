82b40544e1bfc9c145ca095b6d69735276bbf1b9
tensilick <tensilick@proton.me>
2023-01-17 23:07:13 +0000

    added binary

--- nlpiffy.py
+++ nlpiffy.py
@@ -66,4 +66,4 @@ def tokens(text,tokentype,save):
 			click.secho('Sentence Tokens : {}'.format(final_result.sentences),fg='green')
 		else:
 			click.secho('Your text was: {}'.format(text),fg='yellow')
-			click.secho('Word Tokens: {}'.format(final_
+			click.secho('Word Tokens: {}'.format(final_result.words),
