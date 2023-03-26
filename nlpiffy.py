ffd1517d862e13aea86ced7805b2a51cf793e6ae
tensilick <tensilick@proton.me>
2023-03-26 14:17:09 +0000

    Version number.

--- nlpiffy.py
+++ nlpiffy.py
@@ -76,4 +76,4 @@ def tokens(text,tokentype,save):
 	# 		save_to_json(str(final_result.words))
 
 	# 	elif tokentype == 'sentence':
-	# 		click.secho('Your text was: {}'.f
+	# 		click.secho('Your text was: {}'.format(text
