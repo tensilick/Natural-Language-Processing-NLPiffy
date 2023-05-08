63210a29fc8e9484856ccf23c7751076fa94155d
tensilick <tensilick@proton.me>
2023-05-08 20:45:07 +0000

    Version 1.1r36

--- nlpiffy.py
+++ nlpiffy.py
@@ -78,4 +78,5 @@ def tokens(text,tokentype,save):
 	# 	elif tokentype == 'sentence':
 	# 		click.secho('Your text was: {}'.format(text),fg='yellow')
 	# 		click.secho('Sentence Tokens : {}'.format(final_result.sentences),fg='green')
-	# 	el
+	# 	else:
+	
