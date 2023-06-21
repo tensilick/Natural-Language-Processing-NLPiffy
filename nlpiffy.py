69db3fbd953494f62bd642a43eb17f3232305086
tensilick <tensilick@proton.me>
2023-06-21 20:21:19 +0000

    save commit

--- nlpiffy.py
+++ nlpiffy.py
@@ -84,4 +84,4 @@ def tokens(text,tokentype,save):
 	# elif save == 'False':
 	# 	if tokentype == 'word':
 	# 		click.secho('Your text was: {}'.format(text),fg='yellow')
-	# 		click.secho('Word Tokens: {}'.format(final_result.wor
+	# 		click.secho('Word Tokens: {}'.format(final_result.words),fg='gree
