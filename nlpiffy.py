f2cad88961404492c9fb7e3723a40c8275a39841
tensilick <tensilick@proton.me>
2023-06-23 13:00:30 +0000

    Added CSS

--- nlpiffy.py
+++ nlpiffy.py
@@ -87,4 +87,4 @@ def tokens(text,tokentype,save):
 	# 		click.secho('Word Tokens: {}'.format(final_result.words),fg='green')
 	# 	elif tokentype == 'sentence':
 	# 		click.secho('Your text was: {}'.format(text),fg='yellow')
-	# 		click.secho('Sentence Toke
+	# 		click.secho('Sentence Tokens : {}'.format(fin
