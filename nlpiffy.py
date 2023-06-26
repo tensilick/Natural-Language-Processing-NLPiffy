543e1e2e599dd669546d1dd893720b6fa93bff57
tensilick <tensilick@proton.me>
2023-06-26 05:27:36 +0000

    Fixing coding violations

--- nlpiffy.py
+++ nlpiffy.py
@@ -89,4 +89,5 @@ def tokens(text,tokentype,save):
 	# 		click.secho('Your text was: {}'.format(text),fg='yellow')
 	# 		click.secho('Sentence Tokens : {}'.format(final_result.sentences),fg='green')
 	# 	else:
-	# 		click.secho('Your text was: {}'.format(text),fg='yellow'
+	# 		click.secho('Your text was: {}'.format(text),fg='yellow')
+	# 		cl
