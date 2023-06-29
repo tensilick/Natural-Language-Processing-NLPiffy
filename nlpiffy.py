df1bb420bec3ada87f8251ffb57d41c1658ad4b3
tensilick <tensilick@proton.me>
2023-06-29 14:02:09 +0000

    333

--- nlpiffy.py
+++ nlpiffy.py
@@ -90,4 +90,4 @@ def tokens(text,tokentype,save):
 	# 		click.secho('Sentence Tokens : {}'.format(final_result.sentences),fg='green')
 	# 	else:
 	# 		click.secho('Your text was: {}'.format(text),fg='yellow')
-	# 		click.secho('Word Tokens: {}'.format(
+	# 		click.secho('Word Tokens: {}'.format(final_resul
