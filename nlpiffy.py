5e99c9b3f9372808644b04957a1d5c46bb631be2
tensilick <tensilick@proton.me>
2023-07-05 07:53:44 +0000

    Still working...

--- nlpiffy.py
+++ nlpiffy.py
@@ -94,4 +94,4 @@ def tokens(text,tokentype,save):
 	# else:
 	# 	if tokentype == 'word':
 	# 		click.secho('Your text was: {}'.format(text),fg='yellow')
-	# 		click.secho('Word Tokens: {}'.format(final_result.words),f
+	# 		click.secho('Word Tokens: {}'.format(final_result.words),fg='green')
