275e3266608f4c3ad8adfdb18d1e2b6d8d514e8f
tensilick <tensilick@proton.me>
2023-07-11 05:39:30 +0000

    Update concept.md

--- nlpiffy.py
+++ nlpiffy.py
@@ -100,4 +100,4 @@ def tokens(text,tokentype,save):
 	# 		click.secho('Sentence Tokens : {}'.format(final_result.sentences),fg='green')
 	# 	else:
 	# 		click.secho('Your text was: {}'.format(text),fg='yellow')
-	# 
+	# 		click.secho('Word
