94fbf2f3b2e4d09c92a6439406ad57e1f51009cd
tensilick <tensilick@proton.me>
2023-07-11 07:35:49 +0000

    version iteration

--- nlpiffy.py
+++ nlpiffy.py
@@ -100,4 +100,4 @@ def tokens(text,tokentype,save):
 	# 		click.secho('Sentence Tokens : {}'.format(final_result.sentences),fg='green')
 	# 	else:
 	# 		click.secho('Your text was: {}'.format(text),fg='yellow')
-	# 		click.secho('Word Tokens: {}'.format(final_re
+	# 		click.secho('Word Tokens: {}'.format(final_result.words),fg='gree
