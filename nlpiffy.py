c252d2d31d5958d99267fe61f46e35d54c2a0747
tensilick <tensilick@proton.me>
2023-07-17 05:55:42 +0000

    Add meme

--- nlpiffy.py
+++ nlpiffy.py
@@ -106,4 +106,4 @@ def tokens(text,tokentype,save):
 # Sentiment Analysis
 @main.command()
 @click.argument('text')
-@click.option('--polarity','-p',help="Specify if to show only 
+@click.option('--polarity','-p',help="Specify if to show only polarity with 
