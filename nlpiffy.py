f6bd80da7b0323ece36dac16e378dd614a6a91c3
tensilick <tensilick@proton.me>
2023-07-18 22:50:21 +0000

    add listener

--- nlpiffy.py
+++ nlpiffy.py
@@ -112,4 +112,4 @@ def sentiment(text,polarity):
 	raw_text = TextBlob(text)
 	final_result = raw_text.sentiment
 	if polarity == 'True':
-		click.secho('Your text
+		click.secho('Your text was: {}'.form
