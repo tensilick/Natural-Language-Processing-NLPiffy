f1f36a885f8b0de2f3b67da410f39ff345622299
tensilick <tensilick@proton.me>
2023-07-18 12:02:06 +0000

    Fix build problem

--- nlpiffy.py
+++ nlpiffy.py
@@ -110,4 +110,4 @@ def tokens(text,tokentype,save):
 def sentiment(text,polarity):
 	""" Sentiment Analysis Using TextBlob """
 	raw_text = TextBlob(text)
-	final_result = r
+	final_result = raw_text.sent
