c8f0c40e646690a33149976f2a3853b813cf2f28
tensilick <tensilick@proton.me>
2023-07-19 21:19:51 +0000

    Remove binaries

--- nlpiffy.py
+++ nlpiffy.py
@@ -113,4 +113,4 @@ def sentiment(text,polarity):
 	final_result = raw_text.sentiment
 	if polarity == 'True':
 		click.secho('Your text was: {}'.format(text),fg='green')
-		click.secho('Sentiment - Polarity: {}'.format(final_result.po
+		click.secho('Sentiment - Polarity: {}'.format(final_result.polarity),f
