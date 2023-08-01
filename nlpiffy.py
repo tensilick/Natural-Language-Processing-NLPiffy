7f4a7c99a02cd6bab7aa5314335d77c2d24fc342
tensilick <tensilick@proton.me>
2023-08-01 17:38:07 +0000

    update French translation for manual

--- nlpiffy.py
+++ nlpiffy.py
@@ -121,4 +121,5 @@ def sentiment(text,polarity):
 		click.secho('Your text was: {}'.format(text),fg='blue')
 		click.secho('Sentiment: {}'.format(final_result),fg='green')
 
-# Parts of Speech Taggi
+# Parts of Speech Tagging
+@main.comma
