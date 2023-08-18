431f589b5149c0120071df4e5044f0729e11f2a0
tensilick <tensilick@proton.me>
2023-08-18 12:11:36 +0000

    Ran the script

--- nlpiffy.py
+++ nlpiffy.py
@@ -137,4 +137,5 @@ def entities(text):
 	""" Named Entity Recognition Using Spacy """
 	raw_text = nlp(text)
 	final_result_entities = [(entity.text,entity.label_)for entity in raw_text.ents]
-	click.secho('Your text was: {}'.format(text),fg='
+	click.secho('Your text was: {}'.format(text),fg='yellow')
+	cl
