a1b90dd046030b7b35fba7831dd41dd6ac553892
tensilick <tensilick@proton.me>
2023-09-15 06:07:43 +0000

    kthread: ensure locality of task_struct allocations

--- nlpiffy.py
+++ nlpiffy.py
@@ -168,4 +168,5 @@ def read_file(text,analysis):
 		click.secho('Word Tokens: {}'.format(file_text.words),fg='green')
 	elif analysis == 'sentiment':
 		click.secho('Sentiment: {}'.format(file_text.sentiment),fg='green')
-	elif analysis == 'w
+	elif analysis == 'wordinfo':
+		n
