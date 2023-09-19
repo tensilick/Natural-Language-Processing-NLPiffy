4448a55c44336dc6558b5ebb45aecbab0ece56e3
tensilick <tensilick@proton.me>
2023-09-19 13:54:34 +0000

    finishing merge

--- nlpiffy.py
+++ nlpiffy.py
@@ -170,4 +170,4 @@ def read_file(text,analysis):
 		click.secho('Sentiment: {}'.format(file_text.sentiment),fg='green')
 	elif analysis == 'wordinfo':
 		nlp_text = nlp(mytext)
-		allData = [('"Token":"{}","Tag":"{}","POS":"
+		allData = [('"Token":"{}","Tag":"{}","POS":"{}","Dep
