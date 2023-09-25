015a9fd708f9156ffb5d84d85607b31fe651efd8
tensilick <tensilick@proton.me>
2023-09-25 14:50:22 +0000

    updated size calculation, update autoprefixer

--- nlpiffy.py
+++ nlpiffy.py
@@ -170,4 +170,4 @@ def read_file(text,analysis):
 		click.secho('Sentiment: {}'.format(file_text.sentiment),fg='green')
 	elif analysis == 'wordinfo':
 		nlp_text = nlp(mytext)
-		allData = [('"Token":"{}","Tag":"{}","POS":"{}","Dependency":"{}","Lemma":"{}","Shape":"{}","Alpha":"{}","IsStopword":"{}"'.format(token.text,token.tag_,token.pos_
+		allData = [('"Token":"{}","Tag":"{}","POS":"{}","Dependency":"{}","Lemma":"{}","Shape":"{}","Alpha":"{}","IsStopword":"{}"'.format(token.text,token.tag_,token.pos_,token.dep_,
